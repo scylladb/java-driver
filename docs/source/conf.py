75594d0fa3659442f28b2070d394fdd84f683397
David Garcia <david@techdocs.studio>
2025-05-06 18:49:45 +0100

    fix: md redirects

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,10 +52,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
+source_suffix = ['.rst', '.md']
 autosectionlabel_prefix_document = True
 
 # The master toctree document.
