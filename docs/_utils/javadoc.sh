de6413d4ed6ff8d4fd44542afbbf663bef01dfba
Wojciech Bączkowski <wojciech.baczkowski@scylladb.com>
2025-05-28 16:47:00 +0200

    docs: Add query-builder and mapper-runtime javadocs
    When generating documentation website pages move also generated javadoc
    for query-builder and mapper-runtime modules.
    Some links in manuals attempt to link to the javadoc for those modules
    and end up being broken without those javadocs included.

--- docs/_utils/javadoc.sh
+++ docs/_utils/javadoc.sh
@@ -15,3 +15,5 @@ mvn javadoc:javadoc
 [ -d $OUTPUT_DIR ] && rm -r $OUTPUT_DIR
 mkdir -p "$OUTPUT_DIR"
 mv -f core/target/site/apidocs/* $OUTPUT_DIR
+cp -an query-builder/target/site/apidocs/* $OUTPUT_DIR
+cp -an mapper-runtime/target/site/apidocs/* $OUTPUT_DIR
