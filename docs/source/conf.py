0fcb64eab6fa6f9b73135272a751a731fccfe6c3
David Garcia <david@techdocs.studio>
2026-03-12 15:17:29 +0000

    docs: update theme 1.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ author = u'Scylla Project Contributors'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_utils']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_utils', '.venv']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
