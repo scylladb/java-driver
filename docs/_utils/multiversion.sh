f597f09a63fa72d3799728bd4042751cff4ce8e0
David Garcia <david@techdocs.studio>
2026-09-17 16:53:06 +0100

    docs: build each documented branch's javadoc with the JDK it needs (#1004)
    * docs: build each documented branch's javadoc with the JDK it needs
    
    * fix: address coderabbit review
    
    * docs: address comments
    
    harden build and list 3.x as Java 8 versions

--- docs/_utils/multiversion.sh
+++ docs/_utils/multiversion.sh
@@ -1,5 +1,5 @@
-#! /bin/bash	
+#! /bin/bash
 
 cd .. && sphinx-multiversion docs/source docs/_build/dirhtml \
     --pre-build "bash -c \"(find . -mindepth 2 -name README.md -execdir mv '{}' index.md ';'; find . -mindepth 2 -name README.rst -execdir mv '{}' index.rst ';')\"" \
-    --post-build './docs/_utils/javadoc.sh'
+    --post-build "$(pwd)/docs/_utils/javadoc-multiversion.sh"
